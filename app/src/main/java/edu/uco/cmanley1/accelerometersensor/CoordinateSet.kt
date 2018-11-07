94a2fa33a51a56d168493700c97a46c859c226cf
Canaan <canaanm1114@gmail.com>
2018-11-07 16:04:56 -0600

    Complete: -Uploads data to firebase firestore -saves locally to csv
    TODO:
    -When should coordinate list be cleared????

--- app/src/main/java/edu/uco/cmanley1/accelerometersensor/CoordinateSet.kt
+++ app/src/main/java/edu/uco/cmanley1/accelerometersensor/CoordinateSet.kt
@@ -1,7 +1,5 @@
 package edu.uco.cmanley1.accelerometersensor
 
-import java.sql.Timestamp
-
-class CoordinateSet(var x: Float, var y: Float, var z: Float, var timestamp: Timestamp)
+class CoordinateSet(var x: Float, var y: Float, var z: Float, var timestamp: Long)
 {
 }
--- app/src/main/java/edu/uco/cmanley1/accelerometersensor/MainActivity.kt
+++ app/src/main/java/edu/uco/cmanley1/accelerometersensor/MainActivity.kt
@@ -2,7 +2,6 @@ package edu.uco.cmanley1.accelerometersensor
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -12,18 +11,13 @@ import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.FileOutputStream
-import java.io.FileWriter
 import java.sql.Timestamp
-
-const val REQ_CODE_SAVE = 1
 
 class MainActivity : Activity(), SensorEventListener
 {
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometer: Sensor
 
-    private lateinit var csvWriter: FileWriter
     private lateinit var db: FirebaseFirestore
 
     private var coordinateSets = ArrayList<CoordinateSet>()
@@ -41,7 +35,7 @@ class MainActivity : Activity(), SensorEventListener
             accelerometer = it
         }
 
-        grp_delays.setOnCheckedChangeListener { group, checkedId ->
+        grp_delays.setOnCheckedChangeListener { _, checkedId ->
             if(tgl_record.isChecked)
             {
                 sensorManager.unregisterListener(this, accelerometer)
@@ -94,7 +88,7 @@ class MainActivity : Activity(), SensorEventListener
                 sensorManager.unregisterListener(this, accelerometer)
         }
 
-        switch_saveLocal.setOnCheckedChangeListener { buttonView, isChecked ->
+        switch_saveLocal.setOnCheckedChangeListener { _, isChecked ->
             if(isChecked)
             {
                 Toast.makeText(this, R.string.msg_saveLocally,
@@ -116,92 +110,13 @@ class MainActivity : Activity(), SensorEventListener
                 return@setOnClickListener
             }
 
-            var success = true
-            var saveSuccess = true
-            var csv: File? = null
-            if(switch_saveLocal.isChecked)
-            {
-//                csv = File.createTempFile(Timestamp(System.currentTimeMillis()).toString(),
-//                        "csv")
-                csv = File(this.filesDir, "test.csv")
-                csvWriter = FileWriter(csv)
-            }
-
-            for(coordinateSet in coordinateSets)
-            {
-                if(!switch_saveLocal.isChecked)
-                {
-                    db.collection("CoordinateSets")
-                            .document(coordinateSet.timestamp.toString())
-                            .set(coordinateSet)
-                            .addOnFailureListener { ex: Exception ->
-                                Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
-                                success = false
-                            }
-                }
-                else
-                {
-                    try
-                    {
-                        csvWriter.append(coordinateSet.x.toString())
-                        csvWriter.append(",")
-                        csvWriter.append(coordinateSet.y.toString())
-                        csvWriter.append(",")
-                        csvWriter.append(coordinateSet.z.toString())
-                        csvWriter.append(",")
-                        csvWriter.append(coordinateSet.timestamp.toString())
-                        csvWriter.append("\n")
-                    }
-                    catch (ex: Exception)
-                    {
-                        Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
-                        saveSuccess = false
-                    }
-                }
-            }
-            if(success)
-                Toast.makeText(this, R.string.err_uploadSuccess, Toast.LENGTH_SHORT).show()
-            if(saveSuccess)
-            {
-                try
-                {
-                    csvWriter.close()
-                    Toast.makeText(this, csv.toString(), Toast.LENGTH_LONG).show()
-//                    val saveIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
-//                    saveIntent.addCategory(Intent.CATEGORY_OPENABLE)
-//                    saveIntent.putExtra(Intent.EXTRA_STREAM, csv)
-//                    saveIntent.type = "text/csv"
-//
-//                    startActivityForResult(saveIntent, REQ_CODE_SAVE)
-                }
-                catch(ex: Exception)
-                {
-                    Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
-                }
-
-            }
+            if(!switch_saveLocal.isChecked)
+                uploadData()
+            else
+                saveData()
         }
 
     }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
-//    {
-//        if(resultCode == Activity.RESULT_OK)
-//        {
-//            Toast.makeText(this, getString(R.string.err_filePath, data?.data?.toString()),
-//                    Toast.LENGTH_LONG).show()
-//            val pfd = contentResolver.openFileDescriptor(data?.data, "w")
-//            val fileOutputStream = FileOutputStream(pfd.fileDescriptor)
-//            val textContent = data!!.extras[Intent.EXTRA_STREAM].toString()
-//            fileOutputStream.write(textContent.toByteArray())
-//            fileOutputStream.close()
-//            pfd.close()
-//        }
-//        else
-//        {
-//            Toast.makeText(this, R.string.err_saveFail, Toast.LENGTH_SHORT).show()
-//        }
-//    }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int)
     {
@@ -210,24 +125,86 @@ class MainActivity : Activity(), SensorEventListener
 
     override fun onSensorChanged(event: SensorEvent?)
     {
-        if(tgl_record.isChecked)
+        if(tgl_record.isChecked && event != null)
         {
-            val x = event!!.values[0]
-            val y = event!!.values[1]
-            val z = event!!.values[2]
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
 
             txt_xCoord.text = x.toString()
             txt_yCoord.text = y.toString()
             txt_zCoord.text = z.toString()
 
-            try
-            {
-                coordinateSets.add(CoordinateSet(x, y, z, Timestamp(System.currentTimeMillis())))
-            }
-            catch (ex: Exception)
-            {
-                Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
-            }
+            recordData(CoordinateSet(x, y, z, System.currentTimeMillis()))
         }
+    }
+
+    private fun recordData(coordinateSet: CoordinateSet)
+    {
+        try
+        {
+            coordinateSets.add(coordinateSet)
+        }
+        catch (ex: Exception)
+        {
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun uploadData() : Boolean
+    {
+        var success = true
+
+        for(coordinateSet in coordinateSets)
+        {
+            db.collection(getString(R.string.collectionPath))
+                .document(Timestamp(coordinateSet.timestamp).toString())
+                .set(coordinateSet)
+                    .addOnFailureListener { ex: Exception ->
+                        Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
+                        success = false
+                    }
+        }
+
+        if(success)
+            Toast.makeText(this, R.string.err_uploadSuccess, Toast.LENGTH_SHORT).show()
+        return success
+    }
+
+    private fun saveData() : Boolean
+    {
+        var success = true
+
+        try
+        {
+            val path = getExternalFilesDir(null)
+            val dir = File(path, getString(R.string.dir_data))
+
+            if (!dir.exists())
+                dir.mkdirs()
+            val file = File(dir, getString(R.string.file_name,
+                    Timestamp(System.currentTimeMillis()).toString()))
+
+            for (coordinateSet in coordinateSets) {
+                file.appendText(coordinateSet.x.toString())
+                file.appendText(",")
+                file.appendText(coordinateSet.y.toString())
+                file.appendText(",")
+                file.appendText(coordinateSet.z.toString())
+                file.appendText(",")
+                file.appendText(coordinateSet.timestamp.toString())
+                file.appendText("\n")
+            }
+
+            Toast.makeText(this, getString(R.string.err_saveSuccess, file.toString()),
+                    Toast.LENGTH_LONG).show()
+        }
+        catch(ex: Exception)
+        {
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show()
+            success = false
+        }
+
+        return success
     }
 }
